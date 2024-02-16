58008f51d88d1a50d9a473ccca915f00cb392c53
injili <nyarekigospel@gmail.com>
2024-02-16 16:12:26 +0300

    new react app with vite

--- lilibetz/src/App.jsx
+++ lilibetz/src/App.jsx
@@ -1,40 +1,7 @@
-import React, { Component } from "react";
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-} from "react-router-dom";
-import Home from "./components/Home";
-import Dashboard from "./components/Dashboard";
-
 function App() {
-    return (
-        <Router>
-            <div className="bg-zinc-800 text-white mb-2 p-6 text-center">
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/dashboard">Dashboard</Link>
-                    </li>
-                </ul>
-                <Routes>
-                    <Route
-                        exact
-                        path="/"
-                        element={<Home />}
-                    ></Route>
-                    <Route
-                        exact
-                        path="/dashboard"
-                        element={<Dashboard />}
-                    ></Route>
-                </Routes>
-            </div>
-        </Router>
-    );
-};
+  return (
+    <h1 className="">Hello</h1>
+  )
+}
 
-export default App;
+export default App
