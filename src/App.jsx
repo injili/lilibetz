614839ec005e5e46ef30ff9555bd4f1961a9ad27
nyarekigospel@gmail.com <nyarekigospel@gmail.com>
2024-06-10 22:41:17 +0300

    the making of the mistake

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Hero from "./components/hero";
 import Navigation from "./components/navigation";
-import Products from "./components/products";
 
 function App() {
   return (
     <div className="font-DMsans text-zinc-950 bg-stone-100">
       <Navigation/>
       <Hero/>
-      <Products/>
     </div>
   )
 }
