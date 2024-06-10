7b13cdc19f9dcada7c333b533f0e5cf6dd3ac903
nyarekigospel@gmail.com <nyarekigospel@gmail.com>
2024-06-11 02:49:28 +0300

    amazing commit. eeh?

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,4 +1,4 @@
-import { FaRegCopyright, FaFacebookF, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa";
 
 const getFullYear = () => (new Date().getFullYear());
 
@@ -8,28 +8,25 @@ export default function Footer() {
             <div className="m-4 p-8 bg-zinc-950 text-zinc-50 rounded-lg">
                 <div className="flex flex-wrap items-center justify-between  gap-y-4">
                     <span className="flex items-center gap-4 text-xl lg:text-xl xl:text-2xl 2xl:text-4xl font-semibold">
-                        <h1>
-                            <span className="text-amber-400 font-pacifico font-medium">
-                                Lilibetz
-                            </span>  FURNITURE
-                        </h1>
-                        <ul className="flex text-zinc-500 justify-center text-xl gap-2">
-                            <li className="hover:text-amber-400"><FaInstagram/></li>
-                            <li className="hover:text-amber-400"><FaFacebookF/></li>
-                            <li className="hover:text-amber-400"><FaWhatsapp/></li>
-                        </ul>
+                        <div>
+                            <h1>
+                                <span className="text-amber-400 font-pacifico font-medium">
+                                    Lilibetz
+                                </span>  FURNITURE
+                            </h1>
+                        </div>
                     </span>
-                    <div className="flex flex-wrap items-center gap-2">
+                    <div className="flex flex-wrap items-center gap-2 text-sm">
                         <FaRegCopyright/>
                         <p> {getFullYear()} - Lilibetz FURNITURE</p>
                         <p>from INJILI, with Love</p>
                     </div>
-                    <ul className="flex gap-2 md:gap-4  lg:gap-6">
-                        <li>Home</li>
-                        <li>Products</li>
-                        <li>Yard</li>
-                        <li>Contacts</li>
-                        <li>About</li>
+                    <ul className="flex gap-2 md:gap-4 text-sm font-bold lg:gap-6">
+                        <li>HOME</li>
+                        <li>ABOUT</li>
+                        <li>PRODUCTS</li>
+                        <li>CONTACTS</li>
+                        <li>YARD</li>
                     </ul>
                 </div>
             </div>
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,4 +1,5 @@
 import heroImage from '../assets/img/hero.jpg';
+import { FaFacebookF, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 
 function Hero() {
@@ -12,6 +13,13 @@ function Hero() {
                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                     </p>
                 </span>
+                <div className='hidden md:block'>
+                    <div className='flex text-2xl gap-4'>
+                        <FaInstagram/>
+                        <FaFacebookF/>
+                        <FaWhatsapp/>
+                    </div>
+                </div>
             </div>
             <div className="z-10 relative flex overflow-x-hidden overflow-y-hidden">
                 <div className="py-0 animate-marquee whitespace-nowrap">
@@ -31,7 +39,7 @@ function Hero() {
                 </div>
             </div>
             <div className="flex justify-center items-center lg:gap-44"> 
-                    <button className='text-zinc-200 bg-zinc-950 rounded-full h-44 w-44 hidden lg:block'>Scroll Down</button>
+                    <button className='text-zinc-200 text-lg bg-zinc-950 rounded-full h-44 w-44 hidden lg:block'>Scroll Down</button>
                     <span className="font-semibold leading-none lg:text-[200px] text-8xl">decor</span>
             </div>
             <div className='flex justify-center mx-4 my-2 md:my-8'>
